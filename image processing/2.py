927ae1b9dc1e8f8403b2dbe877c18a56bc785e42
as-rajput-11 <73922447+as-rajput-11@users.noreply.github.com>
2022-09-21 17:56:35 +0530

    Create 2.py

---
+++ image processing/2.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import math
+from numpy import matrix
+from numpy import linalg
+
+def rot_x(angle,ptx,pty):
+    return math.cos(angle)*ptx + math.sin(angle)*pty
+
+def rot_y(angle,ptx,pty):
+    return -math.sin(angle)*ptx + math.cos(angle)*pty
+
+angle = math.radians(45)
+im = Image.open('3.jpg')
+(x,y) = im.size
+xextremes = [rot_x(angle,0,0),rot_x(angle,0,y-1),rot_x(angle,x-1,0),rot_x(angle,x-1,y-1)]
+yextremes = [rot_y(angle,0,0),rot_y(angle,0,y-1),rot_y(angle,x-1,0),rot_y(angle,x-1,y-1)]
+mnx = min(xextremes)
+mxx = max(xextremes)
+mny = min(yextremes)
+mxy = max(yextremes)
+print( mnx,mny)
+T = matrix([[math.cos(angle),math.sin(angle),-mnx],[-math.sin(angle),math.cos(angle),-mny],[0,0,1]])
+Tinv = linalg.inv(T);
+print (Tinv)
+Tinvtuple = (Tinv[0,0],Tinv[0,1], Tinv[0,2], Tinv[1,0],Tinv[1,1],Tinv[1,2])
+print (Tinvtuple)
+im = im.transform((int(round(mxx-mnx)),int(round((mxy-mny)))),Image.AFFINE,Tinvtuple,resample=Image.BILINEAR)
+im.save('outputpython2.jpg')
+
+
+
+
+
